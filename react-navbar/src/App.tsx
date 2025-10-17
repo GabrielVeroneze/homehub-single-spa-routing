9db02fa34b4da74a63e0cd34ce4c206008b42301
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-17 16:58:18 -0300

    ✨ feat: adiciona id de integração single-spa ao container principal
    O componente `App` do microfrontend `react-navbar` foi atualizado para incluir um elemento raiz com o id `single-spa-application:react-navbar`. Essa modificação garante que o Single-SPA possa identificar e gerenciar corretamente o ciclo de vida do microfrontend, permitindo seu controle preciso durante a montagem e desmontagem na aplicação principal.

--- react-navbar/src/App.tsx
+++ react-navbar/src/App.tsx
@@ -127,41 +127,46 @@ const App = () => {
     )
 
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{ backgroundColor: '#9C27B0' }}>
-                <Toolbar>
-                    <Button sx={{ padding: 0 }} onClick={toggleDrawer(true)}>
-                        <img
-                            src={HomeHubLogo}
-                            alt="Logotipo do HomeHub"
-                            style={{ width: 176 }}
-                        />
-                    </Button>
-                    <Box sx={{ flexGrow: 1 }} />
-                    <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-                        <IconButton
-                            size="large"
-                            edge="end"
-                            aria-label="account of current user"
-                            aria-haspopup="true"
-                            onClick={handleProfileMenuOpen}
-                            color="inherit"
+        <div id="single-spa-application:react-navbar">
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static" sx={{ backgroundColor: '#9C27B0' }}>
+                    <Toolbar>
+                        <Button
+                            sx={{ padding: 0 }}
+                            onClick={toggleDrawer(true)}
                         >
-                            <AccountCircle />
-                        </IconButton>
-                        <IconButton size="large" color="inherit">
-                            <Badge color="error">
-                                <NotificationsIcon />
-                            </Badge>
-                        </IconButton>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-            <Drawer open={open} onClose={toggleDrawer(false)}>
-                {DrawerList}
-            </Drawer>
-            {renderMenu}
-        </Box>
+                            <img
+                                src={HomeHubLogo}
+                                alt="Logotipo do HomeHub"
+                                style={{ width: 176 }}
+                            />
+                        </Button>
+                        <Box sx={{ flexGrow: 1 }} />
+                        <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+                            <IconButton
+                                size="large"
+                                edge="end"
+                                aria-label="account of current user"
+                                aria-haspopup="true"
+                                onClick={handleProfileMenuOpen}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                            <IconButton size="large" color="inherit">
+                                <Badge color="error">
+                                    <NotificationsIcon />
+                                </Badge>
+                            </IconButton>
+                        </Box>
+                    </Toolbar>
+                </AppBar>
+                <Drawer open={open} onClose={toggleDrawer(false)}>
+                    {DrawerList}
+                </Drawer>
+                {renderMenu}
+            </Box>
+        </div>
     )
 }
 
