a182907af82fd14464f71a622ee414e814e405d0
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-18 18:47:30 -0300

    ✨ feat: adiciona logo e ajustar layout do LoginBox
    Adiciona o logotipo do HomeHub e ajusta o layout do LoginBox para incluir padding, largura máxima e tipografia estilizada.

--- react-login/src/components/LoginBox/index.tsx
+++ react-login/src/components/LoginBox/index.tsx
@@ -1,23 +1,31 @@
 import { Box, Container, Typography } from '@mui/material'
 import LoginForm from '../LoginForm'
+import HomeHubLogo from '../../assets/logo.png'
 
 const LoginBox = () => {
     return (
-        <Container maxWidth="xs">
+        <Container maxWidth={false} sx={{ maxWidth: 578 }} disableGutters>
             <Box
                 sx={{
                     bgcolor: '#F5F5F5',
-                    height: '65vh',
                     mt: '10vh',
                     borderRadius: '24px',
                     display: 'flex',
                     alignItems: 'center',
                     flexDirection: 'column',
+                    p: 7,
+                    pt: 5,
                 }}
             >
-                <Typography>HomeHub</Typography>
-                <Typography>Log In</Typography>
-                <Typography>Insira seus dados para acessar o hub.</Typography>
+                <img src={HomeHubLogo} alt="Logotipo do HomeHub" width={266} />
+                <Box sx={{ mt: 10, width: '100%' }}>
+                    <Typography variant="h5" color="#000000DE">
+                        Log In
+                    </Typography>
+                    <Typography variant="body2" color="#00000099">
+                        Insira seus dados para acessar o hub.
+                    </Typography>
+                </Box>
                 <LoginForm />
             </Box>
         </Container>
