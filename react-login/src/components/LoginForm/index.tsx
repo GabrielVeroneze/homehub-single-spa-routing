5d71c83f7b1da82a3dbb28412d09c1a61f17137c
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-18 18:54:28 -0300

    ✨ feat: melhora LoginForm com lembrar-me e links
    Adiciona checkbox "Lembrar-me" e links para criar conta e recuperar senha, além de ajustes de espaçamento e cores no LoginForm.

--- react-login/src/components/LoginForm/index.tsx
+++ react-login/src/components/LoginForm/index.tsx
@@ -1,13 +1,58 @@
-import { Box, Button, TextField } from '@mui/material'
+import {
+    Box,
+    Button,
+    Checkbox,
+    FormControlLabel,
+    Link,
+    TextField,
+} from '@mui/material'
 
 const LoginForm = () => {
     return (
-        <Box component="form" sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Box
+            component="form"
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+                pt: 4,
+                pb: 2,
+                width: '100%',
+            }}
+        >
             <TextField id="email" label="Email" variant="standard" />
             <TextField id="password" label="Password" variant="standard" />
-            <Button variant="contained" size="large">
+            <FormControlLabel
+                label="Lembrar-me"
+                control={
+                    <Checkbox
+                        defaultChecked
+                        sx={{
+                            '&.Mui-checked': {
+                                color: '#9C27B0',
+                            },
+                        }}
+                    />
+                }
+                sx={{
+                    color: '#00000099',
+                }}
+            />
+            <Button
+                variant="contained"
+                size="large"
+                sx={{ bgcolor: '#9C27B0' }}
+            >
                 Login
             </Button>
+            <Box sx={{ display: 'flex', gap: 3 }}>
+                <Link href="#" variant="body2" color="#9C27B0">
+                    Criar conta
+                </Link>
+                <Link href="#" variant="body2" color="#9C27B0">
+                    Esqueceu a senha?
+                </Link>
+            </Box>
         </Box>
     )
 }
