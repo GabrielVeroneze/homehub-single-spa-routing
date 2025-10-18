97fa32f345c164e9d32a97913be40af1c00ee02b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-18 16:40:39 -0300

    ✨ feat: adiciona campos e botão ao LoginForm
    O componente `LoginForm` agora usa o Material UI, com campos de email e senha e um botão de login, organizados em um layout em coluna.

--- react-login/src/components/LoginForm/index.tsx
+++ react-login/src/components/LoginForm/index.tsx
@@ -1,5 +1,15 @@
+import { Box, Button, TextField } from '@mui/material'
+
 const LoginForm = () => {
-    return <div>Formulário de login</div>
+    return (
+        <Box component="form" sx={{ display: 'flex', flexDirection: 'column' }}>
+            <TextField id="email" label="Email" variant="standard" />
+            <TextField id="password" label="Password" variant="standard" />
+            <Button variant="contained" size="large">
+                Login
+            </Button>
+        </Box>
+    )
 }
 
 export default LoginForm
