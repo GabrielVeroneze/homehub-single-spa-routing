4ac61e0f9e758f2cb7952f74d4346e339293d85d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-18 15:55:45 -0300

    ✨ feat: registra microfrontend react-login no Single-SPA
    Adiciona o registro do microfrontend `@homehub/react-login` na configuração principal do Single-SPA, permitindo que ele seja carregado e gerenciado juntamente com os outros módulos da aplicação.

--- root/src/homehub-root-config.ts
+++ root/src/homehub-root-config.ts
@@ -20,6 +20,16 @@ registerApplication({
     activeWhen: ['/'],
 })
 
+registerApplication({
+    name: '@homehub/react-login',
+    app: () =>
+        import(
+            /* webpackIgnore: true */ // @ts-ignore-next
+            '@homehub/react-login'
+        ),
+    activeWhen: ['/'],
+})
+
 start({
     urlRerouteOnly: true,
 })
