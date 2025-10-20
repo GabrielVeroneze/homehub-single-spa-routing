0aa727515e3c68b5a6f4304f0ef76b8cfa2fe69b
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-10-20 16:22:35 -0300

    ✨ feat: adiciona imagem de fundo à tela de login
    Aplica uma imagem de fundo repetida na parte inferior da página usando o componente Box do MUI, melhorando o layout visual da tela de login.

--- react-login/src/App.tsx
+++ react-login/src/App.tsx
@@ -1,10 +1,20 @@
+import { Box } from '@mui/material'
 import LoginBox from './components/LoginBox'
+import backgroundImg from './assets/background.png'
 
 const App = () => {
     return (
-        <div>
+        <Box
+            sx={{
+                backgroundImage: `url(${backgroundImg})`,
+                backgroundSize: '100%',
+                backgroundRepeat: 'repeat-x',
+                backgroundPosition: 'bottom',
+                height: '90vh',
+            }}
+        >
             <LoginBox />
-        </div>
+        </Box>
     )
 }
 
